d4f16596252c996f200501610ecba61c45b3d5e8
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-07-25 10:01:36 -0700

    Submit Button: barebones styling for the button for now

--- client/src/components/filter/index.js
+++ client/src/components/filter/index.js
@@ -36,7 +36,7 @@ const Filter = (props) => {
           <option value = "saturday">Saturday</option>
         </select>
 
-        <input type = "submit" value = "Submit" />
+        <button className = "submit__btn" onClick = {props.handleSubmit}>Search</button>
         {/* <div>Icons made by <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> */}
       </form>
     </div>
